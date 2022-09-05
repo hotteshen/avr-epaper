7e4697e73c23d7c9a721e38b191daba04aa23610
Korneliusz Osmenda <korneliuszo@gmail.com>
2022-09-05 20:00:39 +0200

    Next attempt

--- generate_image.py
+++ generate_image.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageOps
 from pathlib import Path
 
 im = Image.open("image.png")
-im = im.convert("1")
+im = ImageOps.invert(im.convert("1"))
 
 source = open("fw/image.c","w")
 header = open("fw/image.h","w")
@@ -22,7 +22,7 @@ def gen_image(tgt):
     for x in range(w):
         for y in range(h):
             if(data_raw[y*(w//8)+x//8]&(1<<(7-x%8))):
-                data[x*h//8+y//8] &= ~(1<<(y%8))
+                data[x//8*(h//8)+y] &= ~(1<<(x%8))
     print(len(data))
     line_buffer = []
     for p in data:
